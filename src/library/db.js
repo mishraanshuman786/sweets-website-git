bc5f45e23329da3c4c5c62cb8fc2e3a1c71be921
anshuman619 <mishraanshuman619@gmail.com>
2023-11-25 12:44:08 +0530

    final commit.

--- src/library/db.js
+++ src/library/db.js
@@ -1,5 +1,5 @@
 
 const user=process.env.DB_USER;
 const password=process.env.DB_PASSWORD;
-
-export const connectionSrc="mongodb+srv://"+user+":"+password+"@cluster0.pa8gzbi.mongodb.net/sweetsdb?retryWrites=true&w=majority";
+const verceldb=process.env.MONGODB_URI;
+export const connectionSrc=verceldb;
