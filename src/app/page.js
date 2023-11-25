a60270a5f51b570bd16bc6b16acb1eb2e4486e08
anshuman619 <mishraanshuman619@gmail.com>
2023-11-25 17:31:44 +0530

    final commit.

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,7 @@ export default function Homepage() {
   const [navProducts, setNavProducts] = useState();
 
   async function getData() {
-    let data = await fetch("http://localhost:3000/api/products");
+    let data = await fetch("api/products");
     data = await data.json();
     await setNavProducts(data);
   }
