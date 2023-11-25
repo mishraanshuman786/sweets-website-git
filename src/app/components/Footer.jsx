3c02a9524f86ff78c3e2e53c9e5dc4d35266303e
anshuman619 <mishraanshuman619@gmail.com>
2023-11-26 00:04:32 +0530

    making footer and front page responsive

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ import { FaGlobe } from "react-icons/fa6";
 import { FaPhone } from "react-icons/fa";
 import Link from "next/link";
 import { useState, useEffect } from "react";
-import "./styles/Footer.css";
+// import "./styles/Footer.css";
 export default function Footer(){
 
      //  fetching products from database
@@ -20,39 +20,39 @@ export default function Footer(){
 
 
     return (
-        <div className="footer">
+        <div className="container-fluid row bg-dark">
             {/* Personal Details */}
-          <div className="personal-details" style={{width:"70%"}}>
-            <h1><u>Personal Details</u></h1>
-            <div className="address">
-            <h1> <FaLocationPin style={{marginRight:6}} /> Address: </h1><br/>
-            <p>
+          <div className="col-12 col-xxl-7 ms-xxl-5 mt-xxl-3 ps-xxl-5 pt-5">
+            <h1 className="text-light"><u>Personal Details</u></h1>
+            <div className="mt-4">
+            <h1 className="text-light"> <FaLocationPin style={{marginRight:6}} /> Address: </h1><br/>
+            <p className="text-light h3">
                 S-28/56 R-9 Anula Mahadev Nagar Colony, Varanasi
             </p>
 
             </div>
-            <div className="contact">
-           <h1><FaPhone style={{marginRight:6}} /> Phone Number</h1>
-            <p>6307010388</p>
+            <div className="contact mt-4">
+           <h1 className="text-light"><FaPhone style={{marginRight:6}} /> Phone Number</h1>
+            <p className="text-light h3">6307010388</p>
             </div>
-            <div className="fssai">
-              <h1><FaGlobe style={{marginRight:6}} />FSSAI </h1>
-              <p>22723630000206</p>
+            <div className="fssai mt-4">
+              <h1 className="text-light"><FaGlobe style={{marginRight:6}} />FSSAI </h1>
+              <p className="text-light h3">22723630000206</p>
             </div>
 
           </div>
 
          
           {/* Products List */}
-          <div className="product-list">
-            <h1><u>Our Products</u></h1>
-            <ul>
+          <div className="col-12 col-xxl-4 text-center py-5 pt-2">
+            <h1 className="text-light"><u>Our Products</u></h1>
+            <ul className="list-group list-group-flush" style={{listStyleType:"none"}}>
               {
                 (products)?(
                   products.result.map((element)=>{
                     let url=`/products/${element._id}`;
                       return (
-                        <li key={element._id}><Link className="link" href={url}>{element.productName}</Link></li>
+                        <li key={element._id}><Link  className="text-light list-group-item list-group-item-action list-group-item-success text-dark h3" href={url}>{element.productName}</Link></li>
                       )
                   })
                 ):null
--- src/app/page.js
+++ src/app/page.js
@@ -6,11 +6,7 @@ import Footer from "./components/Footer";
 import "./components/styles/page.css";
 
 export default function Homepage() {
-
-  
-  
   useEffect(() => {
-    
     getData();
   }, []);
 
@@ -23,34 +19,46 @@ export default function Homepage() {
   }
 
   return (
-    <div className="background">
+    <div style={{backgroundColor:"wheat"}}>
       {/* Navbar */}
       <Navbar />
       {/* =================================================================== */}
 
-      <div className="content" >
-        {navProducts
-          ? navProducts.result.map((element) => {
-              let path = `/ProductImages/${element.images[0]}.jpg`;
+      <div className="container bg-success py-2">
+        <div className="row">
+          {navProducts
+            ? navProducts.result.map((element) => {
+                let path = `/ProductImages/${element.images[0]}.jpg`;
 
-              return (
-                <div key={element._id} className="card">
-                  <Image
-                    src={path}
-                    alt="product"
-                    width={180}
-                    height={100}
-                    className="image rounded-md"
-                  />
-                  <h1 className="product-name">{element.productName}</h1>
-                </div>
-              );
-            })
-          : null}
+                return (
+                  <div
+                    className="col-xxl-4 col-xl-6"
+                  >
+                    <div
+                      className="responsive-card card mx-auto bg-dark my-4 w-sm-2"
+                      key={element._id}
+                      style={{width: "25rem",height:"22rem",textAlign:"center"}}
+                    >
+                      <Image
+                        src={path}
+                        alt="product"
+                        width={600}
+                        height={300}
+                        className="card-img-top mx-auto responsive-image"
+                      />
+                      <div class="card-body text-center">
+                        <h5 class="card-title text-light">{element.productName}</h5>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            : null}
+        </div>
       </div>
       <div>
         {/* Footer */}
-        <Footer />
+         <Footer />
       </div>
     </div>
   );
