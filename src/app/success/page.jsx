87d302d2b9ccfbe83835f4e2fd5ad0a49476b967
anshuman619 <mishraanshuman619@gmail.com>
2024-01-29 16:37:50 +0530

    removing error

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/navigation';
+import React from "react";
 
-const Success = () => {
+export const Success = () => {
   const router = useRouter();
   const [countdown, setCountdown] = useState(5);
 
   // Redirect to the home page after 5 seconds
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.push('/'); // Change this to the actual path of your home page
+      router.push("/"); // Change this to the actual path of your home page
     }, 5000);
 
     // Update the countdown every second
@@ -24,8 +23,17 @@ const Success = () => {
   }, [router]);
 
   return (
-    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", marginTop: 100 }}>
-      <h1 style={{ textAlign: "center" }}>Your payment has been done successfully.</h1>
+    <div
+      style={{
+        display: "flex",
+        marginTop: 100,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <h1 style={{ textAlign: "center" }}>
+        Your payment has been done successfully.
+      </h1>
       <p style={{ marginTop: 20, fontSize: 18 }}>
         Redirecting to home page in {countdown} seconds...
       </p>
