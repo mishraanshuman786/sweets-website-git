43bdd2a0c313d4a028308d07ac6ac6fb5fe35f54
anshuman619 <mishraanshuman619@gmail.com>
2024-01-29 16:44:31 +0530

    resolving error

--- src/app/success/page.jsx
+++ src/app/success/page.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+"use client"
+import React,{useEffect,useState} from "react";
+import { useRouter } from "next/navigation";
 
 export const Success = () => {
   const router = useRouter();
