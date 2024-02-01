9d367b7eaf36bfba2a25d4f4dbee41049eb79d69
mishraanshuman786 <125239639+mishraanshuman786@users.noreply.github.com>
2024-02-01 16:00:57 +0530

    todays final commit

--- src/app/success/page.js
+++ src/app/success/page.js
@@ -5,46 +5,59 @@ import axios from "axios";
 import { useRouter } from "next/navigation";
 
 function Success() {
-
-  const backButtonRouter=useRouter();
+  const backButtonRouter = useRouter();
   const [presentPaymentAmount, setPresentPaymentAmount] = useState(null);
   const [presentPaymentAddress, setPresentPaymentAddress] = useState();
   const [presentProductDetails, setPresentProductDetails] = useState();
-  const [presentOrderId,setPresentOrderId]=useState();
-  const [presentUserId,setPresentUserId]=useState();
+  const [presentOrderId, setPresentOrderId] = useState();
+  const [presentUserId, setPresentUserId] = useState();
 
   useEffect(() => {
-    backButtonRouter.replace("/Cart");
+    // Add event listener for the popstate event
+    const handlePopstate = () => {
+      // Run your replace method when the browser back button is clicked
+      backButtonRouter.replace("/Cart");
+    };
+
+    window.addEventListener("popstate", handlePopstate);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("popstate", handlePopstate);
+    };
+  }, [backButtonRouter]);
+
+  useEffect(() => {
     getData();
- 
   }, []);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // saving the data to the order database
-       // Sending the order ID with the axios post request
-       if(presentOrderId && presentUserId && presentPaymentAddress && presentPaymentAmount && presentProductDetails){
-        console.log("api called");
-        const response = await axios.post("/api/orders/payOnline", {
-          userId: presentUserId,
-          orderId:presentOrderId,
-          amount: presentPaymentAmount,
-          paymentAddress: presentPaymentAddress,
-          productDetails:presentProductDetails,
-        });
-  
-        // Handle the response as needed
-        console.log("Axios Response Success Page:", response.data);
-       }else{
-        console.log("api not called");
-       }
-       
+        if (
+          presentOrderId &&
+          presentUserId &&
+          presentPaymentAddress &&
+          presentPaymentAmount &&
+          presentProductDetails
+        ) {
+          const response = await axios.post("/api/orders/payOnline", {
+            userId: presentUserId,
+            orderId: presentOrderId,
+            amount: presentPaymentAmount,
+            paymentAddress: presentPaymentAddress,
+            productDetails: presentProductDetails,
+          });
+
+          console.log("Axios Response Success Page:", response.data);
+        } else {
+          console.log("api not called");
+        }
       } catch (error) {
         console.error("Error fetching data:", error);
       }
     };
-  
+
     fetchData();
   }, [
     presentPaymentAmount,
@@ -53,7 +66,6 @@ function Success() {
     presentOrderId,
     presentUserId,
   ]);
-  
 
   const generateRandomString = (length) => {
     const characters =
@@ -65,55 +77,44 @@ function Success() {
   };
 
   const generateOrderId = () => {
-    const currentDate = new Date().toISOString().slice(0, 10); // Get current date in YYYY-MM-DD format
-    const randomString = generateRandomString(10); // You can adjust the length as needed
+    const currentDate = new Date().toISOString().slice(0, 10);
+    const randomString = generateRandomString(10);
     const orderId = `${currentDate}-${randomString}`;
     return orderId;
   };
 
-
   async function getData() {
     try {
-      // getting the paymentAmount, paymentAddress, productDetails from the localStorage
       let amount = await localStorage.getItem("paymentAmount");
-      let address= await localStorage.getItem("paymentAddress");
-      let details=await localStorage.getItem("productDetails");
+      let address = await localStorage.getItem("paymentAddress");
+      let details = await localStorage.getItem("productDetails");
 
-      // getting the userId
-       // Assuming you have the user ID stored in localStorage under the key 'loginStatus'
-       const storedUserId = JSON.parse(localStorage.getItem("loginStatus"));
+      const storedUserId = JSON.parse(localStorage.getItem("loginStatus"));
 
-      // setting the data in the states
       setPresentPaymentAmount(JSON.parse(amount));
       setPresentPaymentAddress(JSON.parse(address));
       setPresentProductDetails(JSON.parse(details));
       setPresentOrderId(generateOrderId);
       setPresentUserId(storedUserId.data.id);
-
-     
-      
-
-
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   }
 
-  console.log("present Payment Address:",presentPaymentAddress);
-  console.log("present ProductDetails:",presentProductDetails);
-  console.log("present Payment Amount:",presentPaymentAmount);
-  console.log("present User Id:",presentUserId);
-  console.log("present order id:",presentOrderId);
+  console.log("present Payment Address:", presentPaymentAddress);
+  console.log("present ProductDetails:", presentProductDetails);
+  console.log("present Payment Amount:", presentPaymentAmount);
+  console.log("present User Id:", presentUserId);
+  console.log("present order id:", presentOrderId);
 
   return (
     <div className="container">
       {presentPaymentAmount !== null ? (
         <div className="success-message">
-          <img src="/fireworks.gif" alt="fireworks" width={300} height={300}/>
-         <h1> Your Payment is Successful!</h1>
-         <h2>You have purchased the products of Rs{presentPaymentAmount}. </h2>
-         <h2>Thank You For Shopping With US.</h2>
-
+          <img src="/fireworks.gif" alt="fireworks" width={300} height={300} />
+          <h1> Your Payment is Successful!</h1>
+          <h2>You have purchased the products of Rs{presentPaymentAmount}. </h2>
+          <h2>Thank You For Shopping With US.</h2>
         </div>
       ) : (
         <div className="spinner"></div>
