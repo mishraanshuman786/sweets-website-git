a7821b9151e4aa716de3b765cfa8fe2c80a701ab
mishraanshuman786 <125239639+mishraanshuman786@users.noreply.github.com>
2024-02-01 11:32:02 +0530

    handling peice decimal points chn

--- src/app/Cart/page.js
+++ src/app/Cart/page.js
@@ -68,18 +68,18 @@ export default function Cart() {
     // Apply discounts based on categoryId and weight
     switch (categoryId) {
       case "656daabf41ff1afeaba93473":
-        return (price * productWeight * (productWeight >= 1 ? 0.9 : 1)).toFixed(2);
+        return price * productWeight * (productWeight >= 1 ? 0.9 : 1);
       case "656dab9341ff1afeaba93474":
-        return (price * productWeight * (productWeight >= 1 ? 0.85 : 1)).toFixed(2);
+        return price * productWeight * (productWeight >= 1 ? 0.85 : 1);
       case "656dabc341ff1afeaba93475":
-        return (price * productWeight * (productWeight >= 1 ? 0.9 : 1)).toFixed(2);
+        return price * productWeight * (productWeight >= 1 ? 0.9 : 1);
       case "656dabe841ff1afeaba93476":
-        return (price * productWeight * (productWeight >= 1 ? 0.9 : 1)).toFixed(2);
+        return price * productWeight * (productWeight >= 1 ? 0.9 : 1);
       // Add more cases for other categories if needed
   
       default:
         // Default case (no discount)
-        return (price * productWeight).toFixed(2);
+        return price * productWeight;
     }
   };
   
