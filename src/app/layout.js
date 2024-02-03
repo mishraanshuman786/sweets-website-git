6a1a02b0e049d16bae6b6558973c4294aa763ed9
anshuman619 <mishraanshuman619@gmail.com>
2024-02-03 11:33:26 +0530

    layout update

--- src/app/layout.js
+++ src/app/layout.js
@@ -44,14 +44,47 @@ export default function RootLayout({ children }) {
           >
             <p>Powered By NAGINA FOODS</p>
           </div>
+          
 
           <Context>
             <PaymentProvider>
+
+              
               
               {children}
+
+              <div
+        className="fixed-bottom"
+        style={{ marginBottom: 100, marginLeft: 1600, width: 100 }}
+      >
+        {/* WhatsApp content */}
+        <a
+          className="show-tool-tip"
+          href="https://wa.me/916307010388"
+          style={{
+            textDecoration: "none",
+            display: "block",
+            position: "relative",
+          }}
+          target="_blank"
+        >
+          <img
+            src="/whatsapp.svg"
+            alt="whatsapp"
+            className="bounce" // Apply the bounce class here
+            style={{
+              width: 60,
+              height: 60,
+              color: "green",
+              animation: "bounce 1s infinite",
+            }} // Apply the bouncing animation
+          />
+        </a>
+      </div>
               
         
             </PaymentProvider>
+            
           </Context>
         </AuthProvider>
       </body>
