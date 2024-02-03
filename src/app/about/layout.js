e205a78d3da9e177e566f2d0ed57052fdb197af1
anshuman619 <mishraanshuman619@gmail.com>
2024-02-03 17:23:04 +0530

    whatsapp major update

--- src/app/about/layout.js
+++ src/app/about/layout.js
@@ -1,32 +1,30 @@
 import React from "react";
 import Link from "next/link";
+import "../components/styles/whatsappstyle.css";
 
 const Layout = ({ children }) => {
   return (
     <div>
-      <div
-                className="fixed-bottom"
-                style={{ marginBottom: 100, marginLeft: 1600, width: 100 }}
-              >
-                <Link
-                  className="show-tool-tip"
-                  href="https://wa.me/916307010388"
-                  style={{ textDecoration: "none" }}
-                  target="_blank"
-                >
-                  <img
-                    src="/whatsapp.svg"
-                    alt="whatsapp"
-                    className="bounce" // Apply the bounce class here
-                    style={{
-                      width: 60,
-                      height: 60,
-                      color: "green",
-                      animation: "bounce 1s infinite",
-                    }} // Apply the bouncing animation
-                  />
-                </Link>
-              </div>
+      <div className="fixed-bottom whatsappcontainer">
+        <Link
+          className="show-tool-tip"
+          href="https://wa.me/916307010388"
+          style={{ textDecoration: "none" }}
+          target="_blank"
+        >
+          <img
+            src="/whatsapp.svg"
+            alt="whatsapp"
+            className="bounce" // Apply the bounce class here
+            style={{
+              width: 60,
+              height: 60,
+              color: "green",
+              animation: "bounce 1s infinite",
+            }} // Apply the bouncing animation
+          />
+        </Link>
+      </div>
 
       {children}
     </div>
