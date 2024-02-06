c86dad6d0474e33d8415c9e8c4be123e7ca98891
anshuman619 <mishraanshuman619@gmail.com>
2024-02-06 12:51:54 +0530

    content updation

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -120,17 +120,18 @@ function Login() {
               <button type="submit" disabled={buttonDisabled}>
                 Log In
               </button>
+              {/* forgotpassword */}
+            <div className="form-group">
+             <Link href="/forgotpassword"> <button type="submit" disabled={buttonDisabled}>
+                forgot password
+              </button></Link>
               <p style={{ fontSize: 20, fontWeight: "normal", marginTop: 10 }}>
                 If you are not already Registered. Go To{" "}
                 <Link href="/signup">Sign Up Page</Link>
               </p>
             </div>
 
-            {/* forgotpassword */}
-            <div className="form-group">
-             <Link href="/forgotpassword"> <button type="submit" disabled={buttonDisabled}>
-                forgot password
-              </button></Link>
+            
              
             </div>
           </form>
